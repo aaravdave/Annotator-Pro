baff9b9e6d8dcf2610c25fc433e3e707e248360c
Aarav Dave <aarinaarav@gmail.com>
2025-08-17 19:40:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import numpy
+import cv2
+import pyautogui
+import colorsys
+import os
+os.environ['SDL_VIDEO_WINDOW_POS'] = '0, 0'
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
 import pygame
 pygame.init()
 
@@ -6,40 +13,45 @@ def render_colorpicker():
     global z_choose, cpcx, cpcy
 
     pygame.draw.rect(screen, (0, 0, 0), (width // 2 + 40, 30, 295, 345), 0, 5)
-    
+
     for x in range(255):
         for y in range(255):
-            pygame.draw.rect(screen, (x, y, z_choose), (shiftx + x, shifty + y, 1, 1))
+            h = z_choose / 255.0
+            s = x / 255.0
+            v = 1 - (y / 255.0)
+            r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, s, v)]
+            pygame.draw.rect(screen, (r, g, b), (shiftx + x, shifty + y, 1, 1))
             if (x, y, z_choose) == cp_color:
                 cpcx, cpcy = shiftx + x, shifty + y
-    
-    pygame.draw.rect(screen, (0, 0, 0), (cpcx - 5, cpcy - 5, 10, 10), 1)
-    pygame.draw.rect(screen, (255, 255, 255), (cpcx - 4, cpcy - 4, 8, 8), 1)
-    
+
+    pygame.draw.rect(screen, (0, 0, 0), (mouse_x - 5, mouse_y - 5, 10, 10), 1)
+    pygame.draw.rect(screen, (255, 255, 255), (mouse_x - 4, mouse_y - 4, 8, 8), 1)
+
     for z in range(255):
-        pygame.draw.rect(screen, (cp_color[0], cp_color[1], z), (shiftx + z, 325, 1, 20))
-    
+        r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(z / 255.0, 1.0, 1.0)]
+        pygame.draw.rect(screen, (r, g, b), (shiftx + z, 325, 1, 20))
+
     pygame.draw.rect(screen, (0, 0, 0), (shiftx + z_choose - 3, 315, 6, 40), 1)
     pygame.draw.rect(screen, (255, 255, 255), (shiftx + z_choose - 2, 316, 4, 38), 1)
 
 
-import numpy, cv2, pyautogui, os
-os.environ['SDL_VIDEO_WINDOW_POS'] = '0, 0'
 cv2.imwrite('screen.png', cv2.cvtColor(numpy.array(pyautogui.screenshot()), cv2.COLOR_RGB2BGR))
 
 width = pygame.display.Info().current_w
 height = pygame.display.Info().current_h
 image = pygame.transform.scale(pygame.image.load('screen.png'), [width, height])
-PI = 3.141592653589793238462643383279502884197169399375105
 
 screen = pygame.display.set_mode((width, height), flags=pygame.NOFRAME | pygame.FULLSCREEN)
 pygame.display.set_caption('Annotator+ Pro')
 
 tools = list(range(width // 2 - 200, width // 2 - 124, 25)) + list(range(width // 2 + 75, width // 2 + 176, 25))
 coordinates, mouse_x, mouse_y, origin, clicked = [], 0, 0, 0, 0
-current, fill, color, colors = 1, 1, (63, 140, 255), (None, None, None, None, None, (63, 140, 255), (255, 66, 122), (44, 255, 102), None)
+current, fill, color, colors = 1, 1, (63, 140, 255), (
+None, None, None, None, None, (63, 140, 255), (255, 66, 122), (44, 255, 102), None)
 shiftx, shifty = width // 2 + 60, 50
-can, cp_open, cp_color, draw, message, cpcx, cpcy, z_choose, cp_rect, cp_crect, cp_zrect = 1, 0, (255, 255, 255), 0, 'Text...', shiftx + 255, shifty + 255, 0, pygame.Rect(width // 2 + 40, 30, 295, 355), pygame.Rect(width // 2 + 60, 50, 255, 255), pygame.Rect(width // 2 + 60, 325, 255, 20)
+can, cp_open, cp_color, draw, message, cpcx, cpcy, z_choose, cp_rect, cp_crect, cp_zrect = 1, 0, (
+255, 255, 255), 0, 'Text...', shiftx + 255, shifty + 255, 0, pygame.Rect(width // 2 + 40, 30, 295, 355), pygame.Rect(
+    width // 2 + 60, 50, 255, 255), pygame.Rect(width // 2 + 60, 325, 255, 20)
 
 while True:
     for event in pygame.event.get():
@@ -47,41 +59,55 @@ while True:
             quit()
         elif event.type == pygame.MOUSEMOTION:
             mouse_x, mouse_y = event.pos
+
+            if clicked:
+                if cp_open:
+                    if cp_crect.collidepoint(mouse_x, mouse_y):
+                        s = (mouse_x - shiftx) / 255.0
+                        v = 1 - ((mouse_y - shifty) / 255.0)
+                        h = z_choose / 255.0
+                        r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, s, v)]
+                        cp_color = color = (r, g, b)
+                    elif cp_zrect.collidepoint(mouse_x, mouse_y):
+                        z_choose = max(0, min(255, mouse_x - shiftx))
         elif event.type == pygame.MOUSEBUTTONUP:
-            clicked = draw = 0
-            can = 1
+            clicked = 0
+            if cp_open == 0:
+                draw = 0
+                can = 1
         elif event.type == pygame.MOUSEBUTTONDOWN:
             clicked, origin = 1, (mouse_x, mouse_y)
-            if not mouse_y < 30 and cp_open == 0:
-                if current == 4:
-                    if message != 'Text...':
-                        message = 'Text...'
-                        continue
-                draw = 1
-                coordinates.append(
-                    {'ox': origin[0], 'oy': origin[1], 'x': mouse_x, 'y': mouse_y, 'fill': fill, 'color': color,
-                     'tool': current, 'message': message})
-                if current == 4:
-                    message = ''
             if cp_open == 1:
-                cpmx, cpmy = event.pos[0], event.pos[1]
+                cpmx, cpmy = event.pos
                 if not cp_rect.collidepoint(cpmx, cpmy):
                     cp_open = 0
                 else:
                     if cp_crect.collidepoint(cpmx, cpmy):
-                        cp_color = color = (cpmx - (width // 2 + 60), cpmy - 50, z_choose)
+                        s = (cpmx - shiftx) / 255.0
+                        v = 1 - ((cpmy - shifty) / 255.0)
+                        h = z_choose / 255.0
+                        r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, s, v)]
+                        cp_color = color = (r, g, b)
                     elif cp_zrect.collidepoint(cpmx, cpmy):
-                        z_choose = cpmx - (width // 2 + 60)
+                        z_choose = max(0, min(255, cpmx - shiftx))
+            else:
+                if not mouse_y < 30:
+                    draw = 1
+                    coordinates.append({'ox': origin[0], 'oy': origin[1], 'x': mouse_x, 'y': mouse_y,
+                                        'fill': fill, 'color': color, 'tool': current, 'message': message})
         elif event.type == pygame.KEYDOWN:
+            if message == "Text...":
+                message = ""
+
             if pygame.key.get_mods() & pygame.KMOD_SHIFT or pygame.key.get_mods() & pygame.KMOD_CAPS:
                 message += event.unicode
-            elif pygame.key.name(event.key) in [chr(i) for i in range(33, 127)] and message != 'Text...':
+            elif pygame.key.name(event.key) in [chr(i) for i in range(33, 127)]:
                 message += pygame.key.name(event.key)
             elif event.key == pygame.K_f:
                 fill = 0 if fill == 1 else 1
-            elif pygame.key.name(event.key) == 'space':
-                message += ' '
-            elif pygame.key.name(event.key) == 'backspace':
+            elif pygame.key.name(event.key) == "space":
+                message += " "
+            elif pygame.key.name(event.key) == "backspace":
                 message = message[:-1]
 
     screen.blit(image, (0, 0))
@@ -119,11 +145,17 @@ while True:
         if item['tool'] == 1:
             pygame.draw.line(screen, item['color'], (item['ox'], item['oy']), (item['x'], item['y']))
         elif item['tool'] == 2:
-            pygame.draw.polygon(screen, item['color'], ((item['ox'], item['oy']), (item['x'], item['oy']), (item['x'], item['y']), (item['ox'], item['y'])), item['fill'])
+            pygame.draw.polygon(screen, item['color'], (
+            (item['ox'], item['oy']), (item['x'], item['oy']), (item['x'], item['y']), (item['ox'], item['y'])),
+                                item['fill'])
         elif item['tool'] == 3:
-            pygame.draw.arc(screen, item['color'], (item['ox'] if item['ox'] < item['x'] and item['oy'] < item['y'] or item['ox'] < item['x'] and item['y'] < item['oy'] else item['x'], item['oy'] if item['oy'] < item['y'] else item['y'], abs(item['ox'] - item['x']), abs(item['oy'] - item['y'])), 0, 3 * PI, item['fill'])
+            pygame.draw.arc(screen, item['color'], (
+            item['ox'] if item['ox'] < item['x'] and item['oy'] < item['y'] or item['ox'] < item['x'] and item['y'] <
+                          item['oy'] else item['x'], item['oy'] if item['oy'] < item['y'] else item['y'],
+            abs(item['ox'] - item['x']), abs(item['oy'] - item['y'])), 0, 3 * 3.14, item['fill'])
         elif item['tool'] == 4:
-            screen.blit(pygame.font.Font('font.ttc', 32).render(item['message'], False, item['color']), (item['ox'], item['oy']))
+            screen.blit(pygame.font.Font('font.ttc', 32).render(item['message'], False, item['color']),
+                        (item['ox'], item['oy']))
     if draw and not mouse_y < 30:
         coordinates[-1] = {'ox': origin[0], 'oy': origin[1], 'x': mouse_x, 'y': mouse_y, 'fill': fill, 'color': color,
                            'tool': current, 'message': message}
